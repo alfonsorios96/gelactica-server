3af863eaeaac2ce23842e92b744c702552a98dcd
Alfonso Rios <malforime@gmail.com>
2018-01-25 17:31:23 -0600

    Inital commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// importar
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Openpay = require('openpay');
+ 
+// instanciar
+const app = express();
+const openpay = new Openpay('mg5nk23vsodhgb4zjf9w','sk_4b45db1074bb4eb48c9f66312cc946ab');
+
+app.use(cors({
+  origin: '*'
+}));
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/purchase', (request, response) => {
+  let charge = {
+    "source_id": request.body.source_id,
+    "device_session_id": request.body.device_session_id,
+    "customer": {
+        "name": "Customer",
+        "last_name": "Unknown",
+        "phone_number": request.body.customer.phone_number,
+        "email": request.body.customer.email
+    },
+    "currency": request.body.currency,
+    "amount": request.body.amount,
+    "method": request.body.method,
+    "description": request.body.description
+  };
+    openpay.charges.create(charge, (error, body) => {
+      if(error) {
+        response.send(error);
+      } else {
+        response.send(body);
+      }
+    });
+});
+
+app.post('/payout', (request, response) => {
+    var payout = {
+      "method": "bank_account",
+      "bank_account":{
+        "clabe":"012298026516924616",
+        "holder_name": "John Doe"
+      },
+      "amount" : 10.50,
+      "description" : "Monthly payment"
+    };
+    openpay.payouts.create(payout, function (error, body){
+      // ...
+    });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
