4924bf5420f60f044006119a1e0f4704802d440d
Alfonso Rios <manuelalfonso.rios.contractor@bbva.com>
2018-04-15 16:50:37 -0500

    Remove openpay

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const PORT = process.env.PORT || 5000
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Openpay = require('openpay');
 
 const app = express();
-const openpay = new Openpay('mg5nk23vsodhgb4zjf9w','sk_4b45db1074bb4eb48c9f66312cc946ab');
 
 const email = require('./mail');
 
@@ -24,44 +22,5 @@ app.set('view engine', 'html');
 app.set('view engine', 'ejs');
 
 app.post('/email', email.sendEmail);
-
-app.post('/purchase', (request, response) => {
-  let charge = {
-    "source_id": request.body.source_id,
-    "device_session_id": request.body.device_session_id,
-    "customer": {
-        "name": "Customer",
-        "last_name": "Unknown",
-        "phone_number": request.body.customer.phone_number,
-        "email": request.body.customer.email
-    },
-    "currency": request.body.currency,
-    "amount": request.body.amount,
-    "method": request.body.method,
-    "description": request.body.description
-  };
-    openpay.charges.create(charge, (error, body) => {
-      if(error) {
-        response.send(error);
-      } else {
-        response.send(body);
-      }
-    });
-});
-
-app.post('/payout', (request, response) => {
-    let payout = {
-      "method": "bank_account",
-      "bank_account":{
-        "clabe":"012298026516924616",
-        "holder_name": "John Doe"
-      },
-      "amount" : 10.50,
-      "description" : "Monthly payment"
-    };
-    openpay.payouts.create(payout, function (error, body){
-      // ...
-    });
-});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
